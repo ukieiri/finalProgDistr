ba8533c7b413f60a053676ea0a20a9895d29a0a0
ukieiri <ukieiri@gmail.com>
2014-12-01 09:06:40 +0100

    connection to the server with logging

--- JavaProject/src/Exos/ClientConnexionLogging.java
+++ JavaProject/src/Exos/ClientConnexionLogging.java
@@ -14,7 +14,7 @@ import java.util.logging.*;
 
 import Exos.ServerWithLogging.SocketFormatter;
 
-public class ClientConnexionLogging {
+public class ClientConnexionLogging {	
 	//Create a logger
     private final static Logger ClientLogger = Logger.getLogger("ClientLog");;
     
@@ -63,9 +63,7 @@ public class ClientConnexionLogging {
 			//get the current date
 			Calendar currentDate = Calendar.getInstance();
 			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd-H-mm-ss");
-			String dateNow = formatter.format(currentDate.getTime());
-
-	        
+			String dateNow = formatter.format(currentDate.getTime());        
 	        
 			try {
 				
@@ -73,24 +71,24 @@ public class ClientConnexionLogging {
 				//we give a new name every time the program starts,
 				//and we do not append new information
 	            FileHandler fh = new FileHandler(".\\Client" + dateNow + ".log",false);
-
-	            //use our formatter 
-	            SocketFormatter SktFormatter = new SocketFormatter();
-	            fh.setFormatter(SktFormatter);			
-				
+	            
 	            //add the handle to the log            
 	            ClientLogger.addHandler(fh);
 	            ClientLogger.setLevel(Level.INFO);
 	    		ClientLogger.info("*********** program starts ***********");
 
-				//get the server address
-				ServerAddress = InetAddress.getByAddress("",MyIp);
+	            //use our formatter 
+	            SocketFormatter SktFormatter = new SocketFormatter();
+	            fh.setFormatter(SktFormatter);			
+	            
+	        	//get the server address
+	            ServerAddress = InetAddress.getByAddress("",MyIp);
 				System.out.println("Get the address of the server : "+ ServerAddress);
 				ClientLogger.info("Get the address of the server : "+ ServerAddress);
 
 				//try to connect to the server
 				Socket MySocket = new Socket(ServerAddress,45000);
-							    
+
 				System.out.println("We got the connexion to  "+ ServerAddress);
 				ClientLogger.info("We got the connexion to  "+ ServerAddress);
 				
@@ -98,33 +96,38 @@ public class ClientConnexionLogging {
 				BufferedReader Buffin = new BufferedReader (new InputStreamReader (MySocket.getInputStream()));
 				
 				//get an output stream to send data to the server
+				System.out.println("send message to the server...");
 				PrintWriter Pout = new PrintWriter(MySocket.getOutputStream());
+
+				Pout.println("I got the connexion, thanks");
+
+				//tell the server that we have finished to talk
+				Pout.println(""); 
+				Pout.flush();		
 				
 				//listen to the input from the socket
 				//exit when the order quit is given			
 				while(true)
 				{	
-	   			   //Read a line in the buffer, wait until something arrive remove the last cr
-			        System.out.println("wait message from server...");
+					//Read a line in the buffer, wait until something arrive remove the last cr
+					System.out.println("wait message from server...");
 					ClientLogger.info("wait message from server...");
-
+					
 					message_distant = Buffin.readLine().trim();
-		  			Pout.println(message_distant);
-					Pout.flush();		
 
 					//display message received by the server
-			        System.out.println("\nMessage received from server:\n"+message_distant);
+					System.out.println("\nMessage received from server:\n"+message_distant);
 					ClientLogger.info("\nMessage received from server:\n"+message_distant);
-
+					
 					//if quit then exit the loop
-			        if (message_distant.equals("quit"))
-			        {
-			         System.out.println("\nquit sent from server...");
-  				     ClientLogger.info("\nquit sent from server...");
-			         break;
-			        }			        
-				}
-				
+					if (message_distant.equals("quit"))
+					{
+						System.out.println("\nquit sent from server...");
+						ClientLogger.info("\nquit sent from server...");
+						break;
+					}			        
+				}								
+	          
 				//send back the message to the server to kill it
 		        Pout.close();
 		        Buffin.close();
